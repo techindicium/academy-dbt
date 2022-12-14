c6534bfc4db91f4949d5371794f36611691f6e52
Rulyan Fernandes <rulyan.fernandes@indicium.tech>
2022-12-14 19:53:36 +0000

    configurated dbt_project and overridden the schema macro handler

---
+++ macros/generate_schema_name.sql
@@ -0,0 +1,26 @@
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if target.name in ['prod', 'dev'] and custom_schema_name is not none -%}
+
+        {{ custom_schema_name | trim }}
+
+    {%- else -%}
+    
+        {%- if custom_schema_name is none -%}
+
+            {{ default_schema }}
+        
+        {%- elif custom_schema_name == 'sap_adw' -%}
+
+            {{ custom_schema_name | trim }}
+
+        {%- else -%}
+
+            {{ default_schema }}_{{ custom_schema_name | trim }}
+
+        {%- endif -%}
+    
+    {%- endif -%}
+
+{%- endmacro %}
