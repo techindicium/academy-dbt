714a4efb8369372b1e2822a8457d115eef1eabe0
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-01-18 03:02:10 +0000

    feature/stg_sap__address

---
+++ models/staging/sap_adventure/stg_sap__address.sql
@@ -0,0 +1,15 @@
+with
+    source_address as (
+        select
+            cast(addressid as int) as id_address
+            , cast(stateprovinceid as int) as id_stateprovince
+            , cast(city as string) as city
+            , cast(addressline1 as string) as addressline1
+            , cast(addressline2 as string) as addressline2
+            , cast(postalcode as string) as postalcode
+            , cast(spatiallocation as string) as spatiallocation
+            , cast(modifieddate as TIMESTAMP) as modifieddate
+        from {{source('adw', 'address' )}}
+    )
+    select *
+    from source_address
