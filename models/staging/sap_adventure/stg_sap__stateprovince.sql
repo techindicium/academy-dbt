f8202bcdd12c717e5928f6f63e78d46c9beb08fd
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-01-18 03:44:01 +0000

    feature/stg_sap__stateprovince

---
+++ models/staging/sap_adventure/stg_sap__stateprovince.sql
@@ -0,0 +1,15 @@
+with
+    source_stateprovince as (
+        select
+            cast(stateprovinceid as int) as id_stateprovince
+            , cast(territoryid as int) as id_territory
+            , cast(name as string) as state_name
+            , cast(isonlystateprovinceflag as boolean) as isonlystateprovinceflag
+            , cast(stateprovincecode as string) as stateprovincecode
+            , cast(countryregioncode as string) as country_countryregioncode
+            , cast(modifieddate as TIMESTAMP) as modifieddate
+            , cast(rowguid as string) as rowguid
+        from {{source('adw', 'stateprovince' )}}
+    )
+    select *
+    from source_stateprovince
