de9656bc5ec7b8a50d54f2cd4e5dfd7304afd635
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-01-18 00:39:44 +0000

    Alterado cast int para string

--- models/staging/sap_adventure/stg_sap__salesorderhead.sql
+++ models/staging/sap_adventure/stg_sap__salesorderhead.sql
@@ -3,13 +3,13 @@ with
         select
             cast(salesorderid as int) as id_salesorder
             , cast(revisionnumber as int) as revisionnumber 
-            , cast(orderdate as date) as orderdate
-            , cast(duedate as date) as duedate
-            , cast(shipdate as date) as shipdate
+            , cast(orderdate as TIMESTAMP) as orderdate
+            , cast(duedate as TIMESTAMP) as duedate
+            , cast(shipdate as TIMESTAMP) as shipdate
             , cast(status as int) status
             , cast(onlineorderflag as boolean) as onlineorderflag
             , cast(purchaseordernumber as string) as purchaseordernumber
-            , cast(accountnumber as int) as accountnumber
+            , cast(accountnumber as string) as accountnumber
             , cast(customerid as int) as id_customer
             , cast(salespersonid as int) as id_salesperson
             , cast(territoryid as int) as id_territory
@@ -17,15 +17,15 @@ with
             , cast(shiptoaddressid as string) as id_shiptoadress
             , cast(shipmethodid as int) as id_shipmethod
             , cast(creditcardid as int) as id_creditcard
-            , cast(creditcardapprovalcode as int) as creditcardappovalcode
+            , cast(creditcardapprovalcode as string) as creditcardappovalcode
             , cast(currencyrateid as int) as id_currencyrate
             , cast(subtotal as int) as subtotal
             , cast(taxamt as int) as taxamt
             , cast(freight as int) as freight
             , cast(totaldue as int) as totaldue
-            --, cast(comment as 
+            , cast(comment as string) as comment
             , cast(rowguid as string) as rowguid
-            , cast(modifieddate as date) as modifieddate
+            , cast(modifieddate as TIMESTAMP) as modifieddate
         from {{source('adw', 'salesorderheader' )}}
     )
     select *
