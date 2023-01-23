17abf6158aece17ee8f64bf1ee5343d38f0d51ab
Júlio Flávio Pereira Braga <juliopbraga@gmail.com>
2023-01-23 23:53:29 +0000

    Salvando alteracoes em credit card e products

--- models/marts/commercial/dim_products.sql
+++ models/marts/commercial/dim_products.sql
@@ -1,74 +1,31 @@
 with 
-    salesorderhead as (
+    stg_products as (
         select *
-        from {{ ref('stg_sap__salesorderhead') }}
-    )
-
-    , salesorderdetail as (
-        select *
-        from {{ ref('stg_sap__salesorderdetail') }}
-    )
-
-    , products as (
-        select 
-            distinct(id_product)
         from {{ ref('stg_sap__products') }}
     )
+
+    , stg_sub_category as (
+        select *
+        from {{ ref('stg_sap__sub_category') }}
+    )
+
+    , stg_category as (
+        select *
+        from {{ ref('stg_sap__category') }}
+    )
     
-    , joined_tables_detail_head as (
-        select   
-             salesorderdetail.id_salesorderdetail
-            , salesorderdetail.id_product
-            , salesorderdetail.id_specialoffer
-            , salesorderdetail.orderqty
-            , salesorderdetail.unitprice
-            , salesorderdetail.unitpricediscount
-            , salesorderdetail.rowguid
-            , salesorderdetail.modifieddate
-            , salesorderhead.id_salesorder
-            , salesorderhead.id_customer
-            , salesorderhead.id_salesperson
-            , salesorderhead.id_territory
-            , salesorderhead.id_billtoaddress
-            , salesorderhead.id_shiptoaddress
-            , salesorderhead.id_shipmethod
-            , salesorderhead.id_creditcard        
-            , salesorderhead.revisionnumber
-            , salesorderhead.orderdate
-            , salesorderhead.duedate
-            , salesorderhead.shipdate
-            , salesorderhead.status        
-            , salesorderhead.onlineorderflag
-            , salesorderhead.purchaseordernumber
-            , salesorderhead.accountnumber
-            , salesorderhead.creditcardappovalcode
-            , salesorderhead.id_currencyrate        
-            , salesorderhead.subtotal
-            , salesorderhead.taxamt
-            , salesorderhead.freight
-            , salesorderhead.totaldue
-            , salesorderhead.comment
-            , salesorderhead.rowguid
-            , salesorderhead.modifieddate
-        from salesorderhead
-        left join salesorderdetail on
-        salesorderhead.id_salesorder = salesorderdetail.id_salesorder
-    )
-
-    , joined_tables2 as (
+    , joined_tables as (
         select
-          *
-        from products
-        left join joined_tables_detail_head on products.id_product = joined_tables_detail_head.id_salesorderdetail
+            stg_products.blabla 
+            , stg_products.blabla 
+            , stg_products.blabla 
+            , stg_sub_category.sdad
+        from stg_products
+        left join stg_sub_category on
+            stg_products."subcategoryid" = stg_sub_category."subcategoryid"
+        left join stg_category on
+            stg_sub_category."categoryid" = stg_category."categoryid"
     )
-
-   --, transformed as (
-   --  select
-   --    concat(id_product, id_salesorderdetail) as product_sk
-   --  , *
-   --    from joined_tables2
-   --
-   -- )
 
 select *
-from joined_tables2
+from joined_tables
--- models/staging/sap_adventure/stg_sap__address.sql
+++ models/staging/sap_adventure/stg_sap__address.sql
@@ -9,7 +9,7 @@ with
             , cast(postalcode as string) as postalcode
             , cast(spatiallocation as string) as spatiallocation
             , cast(modifieddate as TIMESTAMP) as modifieddate
-        from {{source('adw', 'address' )}}
+        from {{ source('adw', 'address' ) }}
     )
     select *
     from source_address
