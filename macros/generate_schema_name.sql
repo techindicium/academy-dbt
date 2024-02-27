434e40602dae168110f582313ba523d6ca1314d2
juliobraga <juliopbraga@gmail.com>
2024-02-27 14:42:34 -0300

    ajustes dbt_project

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,7 +1,7 @@
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
     {%- set default_schema = target.schema -%}
-    {%- if target.name in ['prod', 'dev'] and custom_schema_name is not none -%}
+    {%- if target.name in ['prod'] and custom_schema_name is not none -%}
 
         {{ custom_schema_name | trim }}
 
